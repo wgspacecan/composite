f2cc3da0dc6876d3c8e3ed6238fc77fcc3b400c2
Will <wgspacecan@gmail.com>
2024-08-31 21:50:10 -0700

    added win detection

--- tic tac toe/trial/will/tic.py
+++ tic tac toe/trial/will/tic.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((500, 820))
+screen = pygame.display.set_mode((470, 820))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -15,13 +15,16 @@ pygame.display.set_caption('Tic Tac Toe')
 
 font = pygame.font.SysFont('Arial', 40)
 
+winner = 0
 objects = []
 states = []
+state1 = []
+state2 = []
 
 for i in range(9):
     states.append(0)
-
-print(states)
+    state1.append(0)
+    state2.append(0)
 
 current_user = 1
 
@@ -85,8 +88,12 @@ class Button():
                         # manage state
                         if states[self.buttonName-1] == 1 and allowRelease:
                             states[self.buttonName-1] = 0
+                            if current_user == 1: state1[self.buttonName-1] = 0
+                            if current_user == 2: state2[self.buttonName-1] = 0
                         elif states[self.buttonName-1] == 0 or allowOverwrite:
                             states[self.buttonName-1] = 1
+                            if current_user == 1: state1[self.buttonName-1] = 1
+                            if current_user == 2: state2[self.buttonName-1] = 1
                             self.toggledUser = current_user
                             self.onclickFunction(self.buttonName)
                 # update press color
@@ -105,8 +112,12 @@ class Button():
         screen.blit(self.buttonSurface, self.buttonRect)
 
 def resetBoard(user):
+    global winner
+    winner = 0
     for i in range(9):
         states[i] = 0
+        state1[i] = 0
+        state2[i] = 0
     print('reset board')
 
 def changeUser(user):
@@ -114,25 +125,59 @@ def changeUser(user):
     current_user = user
     print('changed current user: ' + str(user))
 
+def detectWin(state):
+    result = False
+    if state[0] == state[1] == state[2] == 1: result = True
+    elif state[3] == state[4] == state[5] == 1: result = True
+    elif state[6] == state[7] == state[8] == 1: result = True
+    elif state[2] == state[4] == state[6] == 1: result = True
+    elif state[0] == state[4] == state[8] == 1: result = True
+    elif state[1] == state[4] == state[7] == 1: result = True
+    elif state[3] == state[4] == state[5] == 1: result = True
+    elif state[0] == state[3] == state[6] == 1: result = True
+    elif state[2] == state[5] == state[8] == 1: result = True
+    return result
+
+def detectTie():
+    allOne = True
+    for each in states:
+        if each == 0: allOne = False
+    return allOne
+
+def scanState():
+    global winner
+    if current_user == 1:
+        if detectWin(state1):
+            winner = 1
+            print("User 1 has won!")
+    elif current_user == 2:
+        if detectWin(state2):
+            winner = 2
+            print("User 2 has won!")
+    
+    if detectTie():
+        winner = 9
+        print("TIE")
+
 def buttonPressed(name):
+    scanState()
     if current_user == 1: changeUser(2)
     elif current_user == 2: changeUser(1)
-    print('Button Pressed: ' + str(name))
 
 # first row
-Button(80, 120, 100, 100, 1, buttonPressed)
-Button(190, 120, 100, 100, 2, buttonPressed)
-Button(300, 120, 100, 100, 3, buttonPressed)
+Button(70, 120, 100, 100, 1, buttonPressed)
+Button(180, 120, 100, 100, 2, buttonPressed)
+Button(290, 120, 100, 100, 3, buttonPressed)
 
 # second row
-Button(80, 230, 100, 100, 4, buttonPressed)
-Button(190, 230, 100, 100, 5, buttonPressed)
-Button(300, 230, 100, 100, 6, buttonPressed)
+Button(70, 230, 100, 100, 4, buttonPressed)
+Button(180, 230, 100, 100, 5, buttonPressed)
+Button(290, 230, 100, 100, 6, buttonPressed)
 
 # third row
-Button(80, 340, 100, 100, 7, buttonPressed)
-Button(190, 340, 100, 100, 8, buttonPressed)
-Button(300, 340, 100, 100, 9, buttonPressed)
+Button(70, 340, 100, 100, 7, buttonPressed)
+Button(180, 340, 100, 100, 8, buttonPressed)
+Button(290, 340, 100, 100, 9, buttonPressed)
 
 # menu
 Button(30, 490, 400, 50, 1, changeUser, 'User 1')
@@ -145,13 +190,15 @@ green = (0, 255, 0)
 blue = (0, 0, 128)
 userTextX = 150
 userTextY = 750
-
+# - title
 titleTextX = 150
 titleTextY = 70
-
 text = font.render('TicTacToe!!', True, green, blue)
 textRect = text.get_rect()
 textRect.center = (titleTextX, titleTextY)
+# - winner
+winnerTextX = 200
+winnerTextY = 300
 
 while running:
     # poll for events
@@ -175,6 +222,15 @@ while running:
     # button objects
     for object in objects:
         object.process()
+    
+    # winner text
+    if winner != 0:
+        if winner == 9: message = 'TIE'
+        else: message = 'User ' + str(winner) + ' Wins'
+        winnerText = font.render(message, True, green, blue)
+        winnerRect = winnerText.get_rect()
+        winnerRect.center = (winnerTextX, winnerTextY)
+        screen.blit(winnerText, winnerRect)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
